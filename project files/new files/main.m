cc28cee8ae0677c18a7507b59ce13a05fe54d11d
Ravi Dalmia <ravi.dalmia@gmail.com>
2014-09-15 20:35:35 +0530

    fir commit in feature branch.

---
+++ project files/new files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MMW
+//
+//  Created by Ravi Dalmia on 4/12/13.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
